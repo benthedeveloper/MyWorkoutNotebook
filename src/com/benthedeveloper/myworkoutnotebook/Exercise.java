c1317e61643589eb8a64761a5b3d7f52eece4dc1
Ben Harrison <benjaminpharrison@gmail.com>
2014-02-27 17:07:54 -0800

    Added more code to Workout.java and Exercise.java
    Still working on defining how the objects work together and how they'll
    be saved. I think Workouts will be saved in order by date, and then by
    name, so it will be easy to get the latest workout.

--- src/com/benthedeveloper/myworkoutnotebook/Exercise.java
+++ src/com/benthedeveloper/myworkoutnotebook/Exercise.java
@@ -1,20 +1,38 @@
 package com.benthedeveloper.myworkoutnotebook;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An Exercise consists of a name, a rep-range, a rest time (rest between sets), a 
+ * superSet flag which indicates to do another Exercise immediately after this one, 
+ * and a List of ExSet objects.
+ * @author Ben Harrison
+ *
+ */
 public class Exercise {
 	// Fields
 	String name;  // The name of the Exercise
 	int repRange; // The range of reps for this exercise (i.e. 8-12 reps is MED)
 	int restTime; // The amount of rest between sets (i.e. 30s[VERY_SHORT] to 121s+[VERY_LONG])
 	boolean superSet; // Is this a super-set? (2 exercises in a row with no rest between)
-	boolean triSet;   // Is this a tri-set? (3 exercises in a row with no rest between)
-	Exercise nextExercise; // Reference to the next exercise in a superset/tri-set (if this exercise is one)
 	List<ExSet> sets;  // The list of sets this Exercise consists of
 	
 	// Constructors
-	//TODO
+	public Exercise(String name, int repRange, int restTime, boolean superSet) {
+		this.name = name;
+		this.repRange = repRange;
+		this.restTime = restTime;
+		this.superSet = superSet;
+		sets = new ArrayList<ExSet>();
+	}
 	
 	// Methods
-	//TODO
+	// getName
+	// setName
+	// getRestTime
+	// setRestTime
+	// isSuperSet (if it's a superSet, display the next exercise in the same box)
+	
+	//TODO more methods
 }
--- src/com/benthedeveloper/myworkoutnotebook/Workout.java
+++ src/com/benthedeveloper/myworkoutnotebook/Workout.java
@@ -1,14 +1,16 @@
 package com.benthedeveloper.myworkoutnotebook;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 /**
- * A workout consists of 
+ * A Workout consists of a name, and a list of Exercises perfomed on a specific date.
+ * Two workouts can have the same name.
  * @author Ben Harrison
  *
  */
-public class Workout {
+public class Workout implements Comparable<Workout> {
 	// Fields
 	private String name;               // Name of the Workout
 	private Date date;                 // Date of the Workout (i.e. 3/10/2014)
@@ -16,24 +18,33 @@ public class Workout {
 	
 	// Constructors
 	public Workout(String name, Date date) {
-		this.name = name;
 		this.date = date;
+		this.exercises = new ArrayList<Exercise>();
 	}
 	
 	/**
-	 * Sets the name of the Workout.
-	 * @param newName New name for the workout.
+	 * Gets the name of this Workout.
+	 * @return The name of the Workout.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the name of the Workout to a new name.
+	 * @param newName The new name for the Workout.
 	 */
 	public void setName(String newName) {
 		name = newName;
 	}
 	
 	/**
-	 * Gets the name of the Workout.
-	 * @return The name of the workout.
+	 * Gets the date of this workout.
+	 * @return The date this workout was performed as a Date object.
 	 */
-	public String getName() {
-		return name;
+	public Date getDate() {
+		//TODO: Probably want a method to return a formatted string like 10/15/2014
+		return date;
 	}
 	
 	/**
@@ -41,6 +52,22 @@ public class Workout {
 	 * @param newDate
 	 */
 	public void setDate(Date newDate) {
+		//TODO: Probably want a way to set it to a new date by passing in a
+		// string in the format of: MM/DD/YYYY
 		date = newDate;
+	}
+	
+	/**
+	 * Workouts are sorted by date. If the dates are equal, then they are
+	 * sorted by name.
+	 */
+	public int compareTo(Workout otherWorkout) {
+		//TODO: Comparing dates probably won't work. Probably want to compare
+		// just the year/month/day
+		int dateComp = this.date.compareTo(otherWorkout.date);
+		if (dateComp != 0) // Dates are not equal
+			return dateComp;
+		else
+			return this.name.compareTo(otherWorkout.name);
 	}
 }
