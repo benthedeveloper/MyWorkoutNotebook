8dbd8d3eeb36338c6db4090d739999d1f3f64bec
Ben Harrison <benjaminpharrison@gmail.com>
2014-03-03 23:43:23 -0800

    Added date field to Exercise class.
    Also changed the theme to Android.Holo.Light.

--- src/com/benthedeveloper/myworkoutnotebook/Exercise.java
+++ src/com/benthedeveloper/myworkoutnotebook/Exercise.java
@@ -1,6 +1,7 @@
 package com.benthedeveloper.myworkoutnotebook;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,14 +17,16 @@ public class Exercise {
 	int repRange; // The range of reps for this exercise (i.e. 8-12 reps is MED)
 	int restTime; // The amount of rest between sets (i.e. 30s[VERY_SHORT] to 121s+[VERY_LONG])
 	boolean superSet; // Is this a super-set? (2 exercises in a row with no rest between)
+	Date date;        // The date this exercise was performed
 	List<ExSet> sets;  // The list of sets this Exercise consists of
 	
 	// Constructors
-	public Exercise(String name, int repRange, int restTime, boolean superSet) {
+	public Exercise(String name, int repRange, int restTime, boolean superSet, Date date) {
 		this.name = name;
 		this.repRange = repRange;
 		this.restTime = restTime;
 		this.superSet = superSet;
+		this.date = date;
 		sets = new ArrayList<ExSet>();
 	}
 	
